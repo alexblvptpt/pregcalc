5e2e83ee26be96f4ad7e3d9d25bef25265881a97
alexblvptpt <alex@belov.pt>
2025-06-09 15:09:22 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PregnancyCalculator from './PregnancyCalculator';
+
+function App() {
+  return (
+    <div>
+      <PregnancyCalculator />
+    </div>
+  );
+}
+
+export default App;
